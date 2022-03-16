a63210ff4dd0b010b005461bd9d630f29918d631
Hilal <hilal1993@hotmail.be>
2022-03-16 09:36:49 +0100

    debut du developpement pour rooms

---
+++ app/Models/statutService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class statutService extends Model
+{
+    use HasFactory;
+}
